a1421825ab495874b9f7cf9107a2462ffaa3f2f7
Juhana Räsänen <juhana.rasanen@precond.com>
2018-01-13 14:01:13 +0200

    Add redux-thunk

--- frontend/src/js/app.jsx
+++ frontend/src/js/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+
+import thunk from 'redux-thunk';
 
 import {BrowserRouter as Router} from 'react-router-dom';
 
@@ -16,7 +18,11 @@ import Main from './main';
 // Construct the store with combined initial state and preloaded state from backend
 const preloadedState = window.__INITIAL_STATE__;
 delete window.__INITIAL_STATE__;
-const store = createStore(coreApp, Object.assign(initialState, preloadedState));
+const store = createStore(
+    coreApp,
+    Object.assign(initialState, preloadedState),
+    applyMiddleware(thunk)
+);
 
 
 render(
